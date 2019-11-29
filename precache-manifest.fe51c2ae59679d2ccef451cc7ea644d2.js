ba220ba89d808eddf7de296990bc23053d9fd259
r4pt0s <r4pt0s@protonmail.com>
2019-11-29 11:56:23 +0100

    Updates

---
+++ precache-manifest.fe51c2ae59679d2ccef451cc7ea644d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06ca83c3101362b641f71c07f8cf39f2",
+    "url": "/r4pt0s/robotfriends_react/index.html"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/r4pt0s/robotfriends_react/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "70da96792686afa399c2",
+    "url": "/r4pt0s/robotfriends_react/static/css/main.4120d3ed.chunk.css"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/r4pt0s/robotfriends_react/static/js/2.1f39b60e.chunk.js"
+  },
+  {
+    "revision": "70da96792686afa399c2",
+    "url": "/r4pt0s/robotfriends_react/static/js/main.e7b128dd.chunk.js"
+  },
+  {
+    "revision": "810993ae459eed47a056",
+    "url": "/r4pt0s/robotfriends_react/static/js/runtime-main.2b637af3.js"
+  },
+  {
+    "revision": "35f31c7aa7cc82eaec5d5c37ef2de044",
+    "url": "/r4pt0s/robotfriends_react/static/media/STARWARS.35f31c7a.woff"
+  }
+]);
